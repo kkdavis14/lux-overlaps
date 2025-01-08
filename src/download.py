00228b00ca4c08091cc27fdd98b5749e14bc7e66
wjm55 <william.mattingly@yale.edu>
2025-01-08 18:12:05 -0500

    Refactor download.py to remove obsolete Selenium code and streamline entry extraction
    - Eliminated unused imports related to Selenium and BeautifulSoup.
    - Simplified the entry extraction function by focusing on the `luxy` package's `PeopleGroups` class.

--- src/download.py
+++ src/download.py
@@ -1,12 +1,4 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-import time
-from tqdm import tqdm
 from luxy import PeopleGroups
-# from bs4 import BeautifulSoup
 
 def extract_luxy_entries(query):
     pg = PeopleGroups().filter(name=query, recordType="person").get()
