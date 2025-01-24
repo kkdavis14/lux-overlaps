7f7e6fa2e06bf626db1d41dc3b72037af6502260
Kelly Davis <kelly.davis@yale.edu>
2025-01-24 15:46:47 -0500

    pretty sure this is not going to work...

--- src/download.py
+++ src/download.py
@@ -28,7 +28,9 @@ def create_materialized_view(recordcache, cache):
             jsonb_array_elements(data->'identified_by') AS identified_by,
             jsonb_array_elements(data->'identified_by'->'classified_as') AS classified_as
         FROM {table_name}
-        WHERE data->>'type' = 'Person';
+        WHERE data->>'type' = 'Person'
+        AND jsonb_array_elements(data->'identified_by'->'classified_as')->>'id' = 'http://vocab.getty.edu/aat/300404670';
+
     """
     try:
         with recordcache._cursor(internal=False) as cur:
